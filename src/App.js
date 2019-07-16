70a908369c068f66fa18207c417ef5cd0e42fb48
Charles Chiakwa <charleslukes28@gmail.com>
2019-07-16 04:27:30 +0100

    refactored the app.jsx file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import Card from './component/card'
+import React from 'react';
+import CardList from './component/CardList';
 import './App.css';
 
 function App() {
-  const [robot, setRobot] = useState([]);
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(data => data.json())
-      .then(data => setRobot(robot.concat(data)))
-  }, [])
-
-  console.log(robot)
   return (
     <div className="App">
-      {robot.length ? robot.map(elem => <Card robotImage={elem.id} name={elem.name} email={elem.email} />) :
-        <Card robotImage='sejkre'
-          name='Jane Doe' email='janedoe@gmail.com' />
-      }
+      <CardList />
     </div>
   );
 }
