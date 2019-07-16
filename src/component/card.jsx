85c1cc007ac4b7c1c7ef41b172252e79ac82b56a
Charles Chiakwa <charleslukes28@gmail.com>
2019-07-16 03:52:30 +0100

    add the features for the card

---
+++ src/component/card.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './card.css'
+const Card = (prop) => {
+
+  return (
+    <div className='card-image'>
+      <img src={'https://robohash.org/' + prop.robotImage} alt="The robot"/>
+      <div>
+        <h3>{prop.name}</h3>
+        <p>{prop.email}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
