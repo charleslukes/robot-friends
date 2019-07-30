8b808934737bb41258768c6d86fb8d4b01f387f8
Charles Chiakwa <charleslukes28@gmail.com>
2019-07-30 23:21:29 +0100

    updated thr request robot reducer function

--- src/reducers.js
+++ src/reducers.js
@@ -14,7 +14,7 @@ export const searchRobots = (state = initialStateSearch, action = {}) => {
     case CHANGE_SEARCH_FIELD:
       return Object.assign({}, state, {searchField: action.payload}) 
     default :
-      return state
+      return state;
   } 
 }
 
@@ -29,9 +29,9 @@ export const requestRobots = (state = initialStateRobot, action = {}) => {
     case REQUEST_ROBOT_PENDING:
       return Object.assign({}, state, {isPending: true});
     case REQUEST_ROBOT_SUCCESS:
-      return Object.assign({}, state, {robots: action.payload, isPending: false})
+      return Object.assign({}, state, {robots: action.payload, isPending: false});
     case REQUEST_ROBOT_FAILED:
-      return Object.assign({}, state, {error: action.payload, isPending: false})  
+      return Object.assign({}, state, {error: action.payload, isPending: false});  
     default:
       return state;
   }
