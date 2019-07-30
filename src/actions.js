dacccdd1bd0e239747b58057b6ad5b87bc1619d5
Charles Chiakwa <charleslukes28@gmail.com>
2019-07-30 19:51:42 +0100

    add the action file for redux

---
+++ src/actions.js
@@ -0,0 +1,29 @@
+/**
+ * REDUX
+ * Action => Reducer => Store => React-App
+ * 
+ * Three Principles of redux
+ * 1. Single Source of truth
+ * 2. State is read only
+ * 3. Changes using pure functions
+ */
+
+ import {
+    CHANGE_SEARCH_FIELD,
+    REQUEST_ROBOT_PENDING,
+    REQUEST_ROBOT_SUCCESS,
+    REQUEST_ROBOT_FAILED
+} from './constants';
+
+export const setSearchField = (text) => ({
+  type: CHANGE_SEARCH_FIELD,
+  payload: text
+})
+
+export const requestRobot = (dispatch) => {
+  dispatch({type: REQUEST_ROBOT_PENDING});
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(data => data.json())
+      .then(data => dispatch({ type: REQUEST_ROBOT_SUCCESS, payload: data}))
+      .catch(err => dispatch({ type: REQUEST_ROBOT_FAILED, payload: err}))
+}
