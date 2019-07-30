90f36289c46b2d69ee5dab4c3131837fd4db05b7
Charles Chiakwa <charleslukes28@gmail.com>
2019-07-30 20:15:46 +0100

    combined all the reducers in index file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import { createStore, applyMiddleware} from 'redux';
-import { searchRobots } from './reducers';
+import { createStore, applyMiddleware, combineReducers} from 'redux';
+import { searchRobots, requestRobots } from './reducers';
 import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import './index.css';
@@ -10,9 +10,9 @@ import App from './container/App';
 import * as serviceWorker from './serviceWorker';
 
 
-
+const rootReducer = combineReducers({ searchRobots, requestRobots})
 const logger = createLogger();
-const store = createStore( searchRobots, applyMiddleware(thunkMiddleware, logger) );
+const store = createStore( rootReducer, applyMiddleware(thunkMiddleware, logger) );
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
